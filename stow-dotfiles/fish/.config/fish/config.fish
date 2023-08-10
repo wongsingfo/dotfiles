9dee87f976f46a41abfcc84b9dae7a9d8066806d
Chengke Wang <chengke@pku.edu.cn>
2023-08-10 19:55:37 +0800

    don't change the TERM

--- stow-dotfiles/fish/.config/fish/config.fish
+++ stow-dotfiles/fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 set -x EDITOR nvim
 set -x SHELL fish
 set -x PAGER less
-set -x TERM screen-256color
+
+# Not a good idea to set this.
+# see: https://superuser.com/questions/424086/what-is-the-difference-between-screen-256-color-and-xterm-256color
+# set -x TERM screen-256color
 
 if type -q fzf_configure_bindings
 	fzf_configure_bindings --directory=\co
