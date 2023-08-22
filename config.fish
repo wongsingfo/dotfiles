0f882a2d11afb5f65fa3f6e1d9ae22b0d892922d
Chengke Wang <chengke@pku.edu.cn>
2023-08-22 11:50:22 +0800

    do not enter tmux in fish

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     set -U fish_user_paths ~/.local/bin/ $fish_user_paths
     alias proxyon 'export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890'
 
-    if test -n "$SSH_CONNECTION" -a -z "$TMUX"
-        tmux attach-session -t default || tmux new-session -s default
-    end
+    # if test -n "$SSH_CONNECTION" -a -z "$TMUX"
+    #     tmux attach-session -t default || tmux new-session -s default
+    # end
 end
