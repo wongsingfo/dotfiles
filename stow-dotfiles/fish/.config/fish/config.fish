f648b3a146059e930c30a30175c80dbc57939db9
Chengke Wang <chengke@pku.edu.cn>
2023-10-12 20:45:42 +0800

    update xterm to xterm-256color

--- stow-dotfiles/fish/.config/fish/config.fish
+++ stow-dotfiles/fish/.config/fish/config.fish
@@ -2,9 +2,10 @@ set -x EDITOR nvim
 set -x SHELL fish
 set -x PAGER less
 
-# Not a good idea to set this.
-# see: https://superuser.com/questions/424086/what-is-the-difference-between-screen-256-color-and-xterm-256color
 # set -x TERM screen-256color
+if test "$TERM" = "xterm"
+    set -x TERM "xterm-256color"
+end
 
 if type -q fzf_configure_bindings
 	fzf_configure_bindings --directory=\co
