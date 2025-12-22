61c6f728b3762907e8acda7462b3727299bff954
Chengke Wang <chengke@pku.edu.cn>
2025-12-23 00:20:21 +0800

    Refactor Telescope keybindings and update opencode webfetch permission.

--- stow-dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
+++ stow-dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -34,10 +34,11 @@ return {
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 		vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope recent files' })
 		vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope grep word' })
-		vim.keymap.set('n', '<leader>fs', builtin.git_status, { desc = 'Telescope git status' })
+		vim.keymap.set('n', '<leader>fh', builtin.git_status, { desc = 'Telescope git status' })
+		vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope document symbols (buffer)' })
+		vim.keymap.set('n', '<leader>fS', builtin.lsp_workspace_symbols, { desc = 'Telescope document symbols (workspace)' })
 		vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Telescope resume' })
 	end
 }
